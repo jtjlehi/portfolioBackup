d51423687a8f7d55b7f5651f16e92b1adfabca2e
jared <jared.jacobson.2000@gmail.com>
2018-01-08 18:01:29 -0700

    made the compiler better

--- public/js/test.js
+++ public/js/test.js
@@ -1,3 +1,2 @@
 let myNum = 3;
 let myString = 'string';
-//# sourceMappingURL=/Users/jared/MTECH/inProgress/portfolioSite/public/js/jsMaps/test.js.map
--- ts/test.js
+++
@@ -1,3 +0,0 @@
-let myNum = 3;
-let myString = 'string';
-//# sourceMappingURL=test.js.map
